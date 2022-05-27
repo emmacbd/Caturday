2cb5635788f49fe82e7738280b26098893ddc2ec
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-05-27 16:54:59 -0500

    Add components skeletons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CatContainer from "./CatContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1> CATURDAY </h1>
+        <CatContainer />
+      </main>
     </div>
   );
 }
---
+++ src/CatContainer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CatCards from './CatCards';
+
+
+const CatContainer = () => {
+  return (
+    <div>
+      <h2>hello cool cats and kittens </h2>
+      <CatCards />
+    </div>
+  )
+
+}
+
+
+
+export default CatContainer;
