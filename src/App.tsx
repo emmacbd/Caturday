8a11c0e225b86c3fad2814cff90028d354cb773b
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-05-27 19:13:58 -0500

    Add cat data interface object

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,76 @@ import CatContainer from "./CatContainer";
 import {fetchCats} from "./ApiCalls"
 
 
-interface MyState = {
-  catData: {}[];
+
+interface CatObject {
+  weight: {
+    imperial: string,
+    metric: string
+  },
+  id: string,
+  name: string,
+  cfa_url: string,
+  vetstreet_url: string,
+  vcahospitals_url: string,
+  temperament: string,
+  origin: string,
+  country_codes: string,
+  country_code: string,
+  description: string,
+  life_span: string,
+  indoor: number,
+  lap: number,
+  alt_names: string,
+  adaptability: number,
+  affection_level: number,
+  child_friendly: number,
+  dog_friendly: number,
+  energy_level: number,
+  grooming: number,
+  health_issues: number,
+  intelligence: number,
+  shedding_level: number,
+  social_needs: number,
+  stranger_friendly: number,
+  vocalisation: number,
+  experimental: number,
+  hairless: number,
+  natural: number,
+  rare: number,
+  rex: number,
+  suppressed_tail: number,
+  short_legs: number,
+  wikipedia_url: string,
+  hypoallergenic: number,
+  reference_image_id: string,
+  image: {
+    id: string,
+    width: number,
+    height: number,
+    url: string
+  }
 }
 
-class App extends Component<CatData> {
-    constructor(){
-      super();
+type MyState = {
+  catData: CatObject[]
+}
+
+class App extends Component< {}, MyState> {
       state: MyState = {
-         catData: [],
+         catData:[]
       }
-    }
 
 componentDidMount() {
+  fetchCats()
+  .then(data => this.setState({catData: data}, () => {console.log(this.state)
+          }))
+  .catch(error => {console.log("Oh no!", error)
+    });
+  }
 
-}
 
+
+render() {
   return (
     <div className="App">
       <main>
@@ -27,7 +81,8 @@ componentDidMount() {
         <CatContainer />
       </main>
     </div>
-  );
+  )
+ }
 }
 
 export default App;
