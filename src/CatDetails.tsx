6943a3a6f4047c994e590826543f4e88f03667ee
Angele Williams <angele.williams5@gmail.com>
2022-05-30 17:20:03 -0600

    Refactor Cat of Day logic

--- src/CatDetails.tsx
+++ src/CatDetails.tsx
@@ -15,7 +15,7 @@ const CatDetails: React.FC<CatProps>= ({catInfo}) => {
       <h3>{catInfo.name}</h3>
       <h3>{catInfo.temperament}</h3>
       <h3>{catInfo.id}</h3>
-      <img className="cat-image" src={catInfo.image.url} alt={catInfo.name}/>
+      {catInfo.image && <img className="cat-image" src={catInfo.image.url} alt={catInfo.name}/>}
     </div> : ''
     }
     </>
--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -46,5 +46,5 @@ export interface CatObject {
     wikipedia_url: string;
     hypoallergenic: number;
     reference_image_id: string;
-    image: Image;
+    image?: Image;
 }
