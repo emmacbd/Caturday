09b0053a96f8f7734410ae8a15034c053590c711
Oakley Windiate <oakley.windiate@gmail.com>
2022-06-04 13:07:26 -0600

    Change dropdown menu package to select with options

--- src/CatFilter.tsx
+++ src/CatFilter.tsx
@@ -11,28 +11,27 @@ type CatData = {
 
 const CatFilter: React.FC<CatData> = ({ searchCats }) => {
     const handleSelect = (e: any) => {
-        console.log(e)
-        let num = parseInt(e)
+        console.log(e.target.value)
+        let num = parseInt(e.target.value)
         searchCats(num)
     }
 
     return (
         <div>
-            <DropdownButton
+            <select
                 title="Filter By Affection Level"
                 id="dropdown-menu-align-right"
-                onSelect={handleSelect}
+                onChange={handleSelect}
             >
-                <Dropdown.Item eventKey="7">All Cats</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item eventKey="0">Nope</Dropdown.Item>
-                <Dropdown.Item eventKey="1">Lets You Live In Their House</Dropdown.Item>
-                <Dropdown.Item eventKey="2">You Can Pet Me But Your Friends Can't</Dropdown.Item>
-                <Dropdown.Item eventKey="3">Will Accept Pets In Exchange For Food</Dropdown.Item>
-                <Dropdown.Item eventKey="4">Friendly Within Reason</Dropdown.Item>
-                <Dropdown.Item eventKey="5">I Will Never Leave You</Dropdown.Item>
-                
-            </DropdownButton>
+                <option value="7">All Cats</option>
+                <option value="0">Nope</option>
+                <option value="1">Lets You Live In Their House</option>
+                <option value="2">You Can Pet Me But Your Friends Can't</option>
+                <option value="3">Will Accept Pets In Exchange For Food</option>
+                <option value="4">Friendly Within Reason</option>
+                <option value="5">I Will Never Leave You</option>
+
+            </select>
         </div>
     )
 }
